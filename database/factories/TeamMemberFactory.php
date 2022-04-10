61bd294ec2c2ca07ced3caf1ff7bb3b22b144784
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-10 13:25:56 +0100

    🏭 Using unsplash to generate faces

--- database/factories/TeamMemberFactory.php
+++ database/factories/TeamMemberFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TeamMember>
@@ -16,10 +18,20 @@ class TeamMemberFactory extends Factory
      */
     public function definition()
     {
+        $url = 'https://source.unsplash.com/random/?face';
+        $response = Http::withOptions([
+            'allow_redirects' => false,
+        ])->get($url);
+
+        $location = $response->header('Location');
+        $imageUrl = Str::of($location)->explode('?')[0];
+
+        sleep(1);
+
         return [
             'name' => $this->faker->name(),
             'handle' => $this->faker->domainWord(),
-            'image_url' => $this->faker->url(),
+            'image_url' => $imageUrl,
         ];
     }
 }
