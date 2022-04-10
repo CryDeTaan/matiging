456254f6e0563ae68c072ed5bf13e39d6297afab
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-10 14:18:06 +0100

    🏭 Adding some new lines to the messages.

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\TeamMember;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
@@ -17,9 +18,12 @@ class MessageFactory extends Factory
      */
     public function definition()
     {
+        $text = $this->faker->paragraphs(rand(2, 3), true);
+        $body = Str::of($text)->replace("\n", "<br>");
+
         return [
             'title' => $this->faker->sentence(),
-            'body' => $this->faker->paragraphs(rand(2, 5), true),
+            'body' =>  $body,
             'likes' => $this->faker->numberBetween(50, 250),
             'replies' => $this->faker->numberBetween(0, 50),
             'views' => $this->faker->numberBetween(500, 10000),
