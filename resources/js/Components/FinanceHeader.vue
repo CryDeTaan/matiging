1e6cf9f870fc99540a1c33e8a4a1221302bbf9e7
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-11 19:36:30 +0100

    ✨ Finance page

---
+++ resources/js/Components/FinanceHeader.vue
@@ -0,0 +1,61 @@
+<template>
+    <!-- Page header -->
+    <div class="bg-white shadow rounded-lg">
+        <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div class="py-6 md:flex md:items-center md:justify-between">
+                <div class="flex-1 min-w-0">
+                    <!-- Profile -->
+                    <div class="flex items-center">
+                        <img class="hidden h-16 w-16 rounded-full sm:block"
+                             src="https://images.unsplash.com/photo-1468218457742-ee484fe2fe4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80"
+                             alt=""/>
+                        <div>
+                            <div class="flex items-center">
+                                <h1 class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                                    Good morning, {{ $page.props.user.name }}</h1>
+                            </div>
+                            <dl class="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                                <dt class="sr-only">Company</dt>
+                                <dd class="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
+                                    <OfficeBuildingIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                                        aria-hidden="true"/>
+                                    Duke street studio
+                                </dd>
+                                <dt class="sr-only">Account status</dt>
+                                <dd class="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                                    <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                                                     aria-hidden="true"/>
+                                    Verified Payer
+                                </dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+                    <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Add money
+                    </button>
+                    <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Send money
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {
+    CheckCircleIcon,
+    OfficeBuildingIcon,
+} from '@heroicons/vue/solid'
+
+export default {
+    components: {
+        CheckCircleIcon,
+        OfficeBuildingIcon,
+    },
+}
+</script>
