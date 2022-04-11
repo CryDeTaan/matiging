3f77d527b5658eff466d3c9df38a182b04ed8b91
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-11 18:54:30 +0100

    🚧 Finance View Main component

--- resources/js/Pages/Finance.vue
+++ resources/js/Pages/Finance.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import FinanceMain from "../Components/FinanceMain";
 </script>
 
 <template>
@@ -12,11 +13,7 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                       This is the finance page
-                    </div>
-                </div>
+                <finance-main />
             </div>
         </div>
     </AppLayout>
